0a464e0052b69f162971f4664c89411ed5b02a4d
Matveykylak <matveykylak@gmail.com>
2024-04-27 14:19:28 +0300

    Third task Car

--- HomeWork11/Program.cs
+++ HomeWork11/Program.cs
@@ -1,5 +1,7 @@
 ﻿using HomeWork11.FirstTask;
 using HomeWork11.SecondTask;
+using HomeWork11.ThirdTask;
+using HomeWork11.ThirdTask.Engines;
 //First task
 
 /*MyList<int> list = new MyList<int>() { 1, 2, 3, 4, 5, 1 };
@@ -25,3 +27,41 @@ tmp2.GetInfo();
 Console.WriteLine(" ");*/
 
 //Third task
+
+var dieselEngine = new DieselEngine("tdi3.4", 150, 100, 5.4);
+var petrolEngine = new PetrolEngine("rb42", 240, 70, 10);
+var electricEngine = new ElectricMotor("electro", 500, 40000, 400);
+
+var ford = new Car("mondeo", new DateTime(2006, 05, 04), dieselEngine);
+var nissan = new Car("sky", new DateTime(1999, 06, 05), petrolEngine);
+var tesla = new Car("x", new DateTime(2023, 04, 04), electricEngine);
+
+try
+{
+    ford.Move(100);
+}
+
+catch (EmptyTankException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+try
+{
+    nissan.Move(100);
+}
+
+catch (EmptyTankException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+try
+{
+    tesla.Move(100);
+}
+
+catch (EmptyTankException ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ HomeWork11/ThirdTask/Car.cs
@@ -0,0 +1,39 @@
+﻿
+namespace HomeWork11.ThirdTask
+{
+    public class Car
+    {
+        private string _model;
+        private DateTime _yearOfRelease;
+        public Engine Engine { get; set; }
+
+        public Car(string model, DateTime yearOfRelease, Engine engine)
+        {
+            _model = model;
+            _yearOfRelease = yearOfRelease;
+            Engine = engine;
+        }
+
+        public void Move(double distance)
+        {
+            if (!Engine.UseFuel(distance))
+            {
+                Console.WriteLine("The car can't move");
+            }
+
+            Console.WriteLine($"Lets move");
+        }
+    }
+}
+
+
+
+
+
+
+
+/*
+Реализовать класс машина у который будет поле обобщенное двигатель. Создать
+иерархию наследования для двигателей (абстрактный, дизельный, бензиновый,
+електро). Сделать так чтобы создать автомобиль можно было только передавая
+туда один из типов двигателя. Реализовать методы для движения автомобиля.*/
